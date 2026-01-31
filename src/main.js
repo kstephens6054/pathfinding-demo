8132fb3fa2d4244052ed8ca6a61d11dcafe30fb8
Kurt Stephens <kstephens6054@gmail.com>
2026-01-31 01:51:55 +0000

    Added GridDisplay component class

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 import "./style.css";
-import * as _drawableGridModule from "./DrawableGrid";
+import * as _GridDisplayModule from "./GridDisplay/GridDisplay.js";
 
 document.querySelector("#app").innerHTML = `
   <div class="container">
     <h1>Drawable Grid Demo</h1>
-    <div id="grid-container">
-      <drawable-grid
+      <grid-display
         width="320"
         height="240"
         rows="24"
         columns="32"
-        pen-color="#FFCC00"
         background-color="#E0E0E0"
         grid-color="#7F8AC0"
-        show-grid="false"
-      ></drawable-grid>
-    </div>
+        show-grid="true"
+      ></grid-display>
   </div>
 `;
