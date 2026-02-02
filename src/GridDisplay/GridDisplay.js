333925002582ff7d8628e8a24639838059bc6c46
Kurt Stephens <kstephens6054@gmail.com>
2026-02-02 03:57:16 +0000

    Added setters/getters and lifecycle methods to GridDisplay

--- src/GridDisplay/GridDisplay.js
+++ src/GridDisplay/GridDisplay.js
@@ -37,14 +37,26 @@ class GridDisplay extends HTMLElement {
     root.adoptedStyleSheets = [GridDisplayStyles];
     root.innerHTML = GridDisplay._innerHTML;
 
+    this._width = GridDisplay.defaults.width;
+    this._height = GridDisplay.defaults.height;
+    this._rows = GridDisplay.defaults.rows;
+    this._columns = GridDisplay.defaults.columns;
+    this._backgroundColor = GridDisplay.defaults.backgroundColor;
+    this._gridColor = GridDisplay.defaults.gridColor;
+    this._showGrid = GridDisplay.defaults.showGrid;
+
     this._canvas = root.querySelector("#canvas");
     this._context = this._canvas.getContext("2d");
     this._canvasWrapper = root.querySelector("#canvas-wrapper");
     this._gridData = null;
   }
 
+  /**
+   * Getters and Setters
+   */
+
   get width() {
-    return parseInt(this.getAttribute("width")) || GridDisplay.defaults.width;
+    return parseInt(this.getAttribute("width")) || this._width;
   }
 
   set width(value) {
@@ -56,7 +68,7 @@ class GridDisplay extends HTMLElement {
   }
 
   get height() {
-    return parseInt(this.getAttribute("height")) || GridDisplay.defaults.height;
+    return parseInt(this.getAttribute("height")) || this._height;
   }
 
   set height(value) {
@@ -68,7 +80,7 @@ class GridDisplay extends HTMLElement {
   }
 
   get rows() {
-    return parseInt(this.getAttribute("rows")) || GridDisplay.defaults.rows;
+    return parseInt(this.getAttribute("rows")) || this._rows;
   }
 
   set rows(value) {
@@ -80,9 +92,7 @@ class GridDisplay extends HTMLElement {
   }
 
   get columns() {
-    return (
-      parseInt(this.getAttribute("columns")) || GridDisplay.defaults.columns
-    );
+    return parseInt(this.getAttribute("columns")) || this._columns;
   }
 
   set columns(value) {
@@ -94,10 +104,7 @@ class GridDisplay extends HTMLElement {
   }
 
   get backgroundColor() {
-    return (
-      this.getAttribute("background-color") ||
-      GridDisplay.defaults.backgroundColor
-    );
+    return this.getAttribute("background-color") || this._backgroundColor;
   }
 
   set backgroundColor(value) {
@@ -105,7 +112,7 @@ class GridDisplay extends HTMLElement {
   }
 
   get gridColor() {
-    return this.getAttribute("grid-color") || GridDisplay.defaults.gridColor;
+    return this.getAttribute("grid-color") || this._gridColor;
   }
 
   set gridColor(value) {
@@ -115,7 +122,7 @@ class GridDisplay extends HTMLElement {
   get showGrid() {
     const show = this.getAttribute("show-grid");
     if (show === null) {
-      return GridDisplay.defaults.showGrid;
+      return this._showGrid;
     }
     return show === "true";
   }
@@ -124,6 +131,78 @@ class GridDisplay extends HTMLElement {
     this.setAttribute(
       "show-grid",
       value === true || value === "true" ? "true" : "false",
+    );
+  }
+
+  /**
+   * Lifecycle Callbacks
+   */
+
+  connectedCallback() {
+    this._updateCanvas();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    const actions = new Map([
+      ["width", this._updateWidth],
+      ["height", this._updateHeight],
+      ["rows", this._updateRows],
+      ["columns", this._updateColumns],
+    ]);
+
+    if (oldValue !== newValue) {
+      const action = actions.get(name);
+      if (action) {
+        action.call(this, oldValue, newValue);
+      }
+
+      this._updateCanvas();
+    }
+  }
+
+  /**
+   * Private Methods
+   */
+
+  _updateWidth(oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    const width = parseInt(newValue);
+    this._canvas.width = width;
+    this._canvasWrapper.style.width = `${width}px`;
+  }
+
+  _updateHeight(oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    const height = parseInt(newValue);
+    this._canvas.height = height;
+    this._canvasWrapper.style.height = `${height}px`;
+  }
+
+  _updateRows(oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    this._rows = parseInt(newValue);
+  }
+
+  _updateColumns(oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    this._columns = parseInt(newValue);
+  }
+
+  _clearGridData() {
+    this._gridData = Array.from({ length: this._rows }, () =>
+      Array.from({ length: this._columns }, () => this._backgroundColor),
     );
   }
 }
